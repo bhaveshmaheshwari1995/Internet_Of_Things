7ef6f34d29295c68b49c41513143841c2e75ab58
Bhavesh Maheshwari <bhavesh.maheshwari08@gmail.com>
2017-04-04 00:40:25 +0530

    Update config.js
    removed aws credentials

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 		secret: 'smartparksecret',
         url : 'mongodb://localhost/smartpark',
-        s3keyId:"AKIAJTK3ZCP2HK2HBHCA",
-		s3SecretKey:"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",
+        s3keyId:"",
+		s3SecretKey:"",
 		chargePerMinute:.5,
     }
 
