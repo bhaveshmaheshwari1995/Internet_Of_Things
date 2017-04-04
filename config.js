913faa9d3701e5ff477667881452aa696a4bc69b
Ameya Shukla <ameya.shukla5@gmail.com>
2017-04-04 09:23:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 module.exports = {
 		secret: 'smartparksecret',
         url : 'mongodb://localhost/smartpark',
-        s3keyId:"AKIAJTK3ZCP2HK2HBHCA",
-		s3SecretKey:"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",
 		chargePerMinute:.5,
     }
 
